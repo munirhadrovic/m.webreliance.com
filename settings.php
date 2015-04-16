4f20080bdc3100190f3eb5764431620143e6a288
Emir <emiratesbp@gmail.com>
2015-04-16 13:11:28 +0200

    fisrt commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="1381 W US Highway 22 and 3"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Maineville, OH 45039";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=5133252640;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
